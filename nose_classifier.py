d4367e7373df378604ffec29f48e2e8446f754f6
Shiqi Sheng <shiqisheng00@gmail.com>
2021-04-25 19:55:38 -0600

    feat: added driver to train neural network
    From part1

--- nose_classifier.py
+++ nose_classifier.py
@@ -146,3 +146,72 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate,
 ##############################################################################
 #                             END OF YOUR CODE                               #
 ##############################################################################
+
+# Code to train the neural net
+%%time
+def train(model, trainloader, valloader, num_epoch = 10): # Train the model
+  print("Start training...")
+  trn_loss_hist = []
+  trn_acc_hist = []
+  val_acc_hist = []
+  model.train() # Set the model to training mode
+  for i in range(num_epoch):
+    running_loss = []
+    print('-----------------Epoch = %d-----------------' % (i+1))
+    for batch, label in tqdm(trainloader):
+      batch = batch.to(device)
+      label = label.to(device)
+      optimizer.zero_grad() # Clear gradients from the previous iteration
+      pred = model(batch) # This will call Network.forward() that you implement
+      loss = criterion(pred, label) # Calculate the loss
+      running_loss.append(loss.item())
+      loss.backward() # Backprop gradients to all tensors in the network
+      optimizer.step() # Update trainable weights
+    print("\n Epoch {} loss:{}".format(i+1,np.mean(running_loss)))
+
+    # Keep track of training loss, accuracy, and validation loss
+    trn_loss_hist.append(np.mean(running_loss))
+    trn_acc_hist.append(evaluate(model, trainloader))
+    print("\n Evaluate on validation set...")
+    val_acc_hist.append(evaluate(model, valloader))
+  print("Done!")
+  return trn_loss_hist, trn_acc_hist, val_acc_hist
+
+def evaluate(model, loader): # Evaluate accuracy on validation / test set
+  model.eval() # Set the model to evaluation mode
+  correct = 0
+  with torch.no_grad(): # Do not calculate grident to speed up computation
+    for batch, label in tqdm(loader):
+      batch = batch.to(device)
+      label = label.to(device)
+      pred = model(batch)
+      correct += (torch.argmax(pred,dim=1)==label).sum().item()
+    acc = correct/len(loader.dataset)
+    print("\n Evaluation accuracy: {}".format(acc))
+    return acc
+    
+trn_loss_hist, trn_acc_hist, val_acc_hist = train(model, trainloader, 
+                                                  valloader, num_epoch)
+
+##############################################################################
+# TODO: Note down the evaluation accuracy on test set                        #
+##############################################################################
+print("\n Evaluate on test set")
+evaluate(model, testloader);
+
+##############################################################################
+# TODO: Submit the accuracy plot                                             #
+##############################################################################
+# visualize the training / validation accuracies
+x = np.arange(num_epoch)
+# train/val accuracies for MiniVGG
+plt.figure()
+plt.plot(x, trn_acc_hist)
+plt.plot(x, val_acc_hist)
+plt.legend(['Training', 'Validation'])
+plt.xticks(x)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('fashion MNIST Classification')
+plt.gcf().set_size_inches(10, 5)
+plt.show()
